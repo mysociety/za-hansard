cac6eaf1b2daf5cd3f5d741844dbc3757bfb85a8
Hakim Cassimally <hakim.cassimally@gmail.com>
2013-06-28 17:46:11 +0100

    Implemented parsers: isTitle, assembled, prayers
    also refactored parsers to mutate header/root xml object directly,
    as this is more convenient than previous version.

--- zah/parse.py
+++ zah/parse.py
@@ -33,11 +33,10 @@ class ZAHansardParser(object):
                     E.preface()))
         current = akomaNtoso.debate.preface
 
-        name_regexp = r'((?:[A-Z][a-z]* )?[A-Z ]+):'
-
         header = {
                 'date': None,
                 'title': None,
+                'subSections': 0,
                 'assembled': None,
                 'prayers': False,
                 }
@@ -54,34 +53,36 @@ class ZAHansardParser(object):
         groups = groupby(lines, break_paras)
         paras = imap(snd, ifilter(fst, groups))
 
-        for p in paras:
-            elem_data = self.get_element( list(p), header.copy() )
-            if not elem_data:
-                continue
-            (elem, where, header) = elem_data
-            if where == 'pop':
-                current = current.getparent()
-            parent = current
-
-            current.append(elem)
-            if where == 'push':
-                current = elem
+        for para in paras:
+            p = list(para)
+            ret = self.parseLine( p, header, akomaNtoso, current )
+            if not ret:
+                print >> sys.stderr, "Failed at %s" % p[0]
+                break
+                # continue
+            [current] = ret
 
         return akomaNtoso
 
-    def get_element(self, p, header):
+    def parseLine(self, p, header, akomaNtoso, current):
 
         E = self.E
 
+        # DECORATORS
         def singleLine(f):
-            def new_f(p, header):
+            def singleLine_(p, header, akomaNtoso, current):
                 if len(p) != 1:
                     return False
-                return f(p[0], header)
-            return new_f
+                return f(p[0], header, akomaNtoso, current)
+            return singleLine_
+
+        def para(f):
+            def para_(p, header, akomaNtoso, current):
+                return f(' '.join(p), header, akomaNtoso, current)
+            return para_
 
         @singleLine
-        def isDate(line,header):
+        def isDate(line, header, akomaNtoso, current):
             if header['date']:
                 return False
             try:
@@ -91,16 +92,77 @@ class ZAHansardParser(object):
                         datetime.strftime(date, '%A, '),
                         E.docDate(datetime.strftime(date, '%d %B %Y'),
                             date=datetime.strftime(date, '%Y-%m-%d')))
-                return (elem, None, header)
-            except Exception as e:
-                print e
+                current.append(elem)
+                return [current]
+            except:
                 return False
+
+        @singleLine
+        def isTitle(line, header, akomaNtoso, current):
+            if re.search(r'[a-z]', line):
+                return False
+            line = line.lstrip()
+            if header['title']:
+                # we already have a main title, so this is a subsection
+                if header['subSections']:
+                    current = current.getparent()
+                header['subSections'] += 1
+                elem = E.debateSection(
+                    E.narrative(line),
+                    id='dbs%d' % header['subSections'])
+                current.append(elem)
+                return [elem]
+            else:
+                header['title'] = line
+                elem = E.debateBody(
+                        E.debateSection(
+                            E.heading(line),
+                            id='db0'))
+                akomaNtoso.debate.append( elem )
+                return [elem.debateSection]
+
+        @para
+        def assembled(p, header, akomaNtoso, current):
+            if header['assembled']:
+                return
+            ret = re.search(r'^(.*(?:assembled|met)(?: in .*)? at )(\d+:\d+)\.?$', p)
+            if ret:
+                try:
+                    groups = ret.groups()
+                    time = datetime.strptime(groups[1], '%H:%M').time()
+                    assembled = datetime.combine(header['date'], time)
+                    header['assembled'] = assembled
+                    elem = E.p(
+                            groups[0],
+                            E.recordedTime(
+                                groups[1],
+                                time= assembled.isoformat()
+                            ))
+                    current.append(elem)
+                    return [current]
+                except:
+                    return
+
+        @para
+        def prayers(p, header, akomaNtoso, current):
+            if header['prayers']:
+                return
+            if re.search(r'^(.* prayers or meditation.)$', p):
+                header['prayers'] = True
+                elem = E.prayers(p)
+                current.append(elem)
+                return [current]
+
+        name_regexp = r'((?:[A-Z][a-z]* )?[A-Z ]+):'
 
         funcs = [
                 isDate,
+                isTitle,
+                assembled,
+                prayers,
                 ]
 
         for f in funcs:
-            ret = f(p, header)
+            ret = f(p, header, akomaNtoso, current)
             if ret:
                 return ret

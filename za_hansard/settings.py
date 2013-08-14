ac4478a403bd9bfa554a0bcd9432f8e182898af4
Hakim Cassimally <hakim.cassimally@gmail.com>
2013-08-14 15:12:57 +0100

    fixing PROJECT_ROOT for settings.py layout of za_hansard

--- za_hansard/settings.py
+++ za_hansard/settings.py
@@ -6,8 +6,7 @@ import yaml
 # Path to here is something like
 # /data/vhost/<vhost>/<repo>/<project_name>/settings/base.py
 SETTINGS_DIR = os.path.abspath(os.path.dirname(__file__))
-PROJECT_DIR = os.path.abspath(os.path.join(SETTINGS_DIR, '..'))
-PROJECT_ROOT = os.path.abspath(os.path.join(PROJECT_DIR, '..'))
+PROJECT_ROOT = os.path.abspath(os.path.join(SETTINGS_DIR, '..'))
 PARENT_DIR = os.path.abspath(os.path.join(PROJECT_ROOT, '..'))
 
 config_file = os.path.join(PROJECT_ROOT, 'conf', 'general.yml')

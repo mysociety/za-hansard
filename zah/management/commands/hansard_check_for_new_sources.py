c7cd79c699cc99eb008483bf7248945f4aac42f1
Hakim Cassimally <hakim@mysociety.org>
2013-09-09 15:37:54 +0000

    Improve scraping command
    Reverse sources list, so that ids are created sequentially
    Removed start parameter, which is mostly useless
    Renamed end parameter to --limit option, which is also mostly useless
    (except perhaps for testing purposes)
    Add --stop-on-seen option which will be useful when running in cron
    Better checks to prevent infinite processing without --limit option

--- zah/management/commands/hansard_check_for_new_sources.py
+++ zah/management/commands/hansard_check_for_new_sources.py
@@ -5,6 +5,7 @@ import datetime
 import time
 import sys
 
+from optparse import make_option
 from bs4 import BeautifulSoup
 
 from django.conf import settings
@@ -18,68 +19,104 @@ class FailedToRetrieveSourceException (Exception):
     pass
 
 class Command(BaseCommand):
-    args = '<start end>'
     help = 'Check for new sources'
-
-
-    def handle(self, *args, **options):
-        (start, end) = [int(x) for x in args]
-        self.retrieve_sources(start, end)
-
-    def retrieve_sources(self, start, end):
-
-        url = 'http://www.parliament.gov.za/live/content.php?Category_ID=119&DocumentStart=%d' % (start or 0)
-        self.stdout.write("Retrieving %s" % url)
-        h = httplib2.Http( settings.HTTPLIB2_CACHE_DIR )
-        response, content = h.request(url)
-        assert response.status == 200
-        self.stdout.write("OK")
-        # content = open('test.html').read()
-
-        # parse content
-        soup = BeautifulSoup(
-            content,
-            'xml',
+    option_list = BaseCommand.option_list + (
+        make_option('--stop-on-seen',
+            default=False,
+            action='store_true',
+            help='Stop when get as far back as first seen item',
+        ),
+        make_option('--limit',
+            default=0,
+            type='int',
+            help='Limit number of entries checked'),
         )
 
-        rx = re.compile(r'Displaying (\d+)  (\d+) of the most recent (\d+)')
+    def handle(self, *args, **options):
+        sources = self.retrieve_sources(0, options)
+        sources.reverse()
+        sources_db = [Source.objects.get_or_create(**source) for source in sources]
+        sources_count = len(sources)
+        created_count = sum([1 for (_,created) in sources_db if created])
+        self.stdout.write('Sources found: %d\nSources created: %d\n' % (
+            sources_count, created_count))
 
-        pager = soup.find('td', text=rx)
-        match = rx.search(pager.text)
-        (pstart, pend, ptotal) = [int(p) for p in match.groups()]
+    def retrieve_sources(self, start, options):
 
-        self.stdout.write( "Processing %d to %d" % (pstart, pend) )
+        try:
+            url = 'http://www.parliament.gov.za/live/content.php?Category_ID=119&DocumentStart=%d' % (start or 0)
+            self.stdout.write("Retrieving %s" % url)
+            h = httplib2.Http( settings.HTTPLIB2_CACHE_DIR )
+            response, content = h.request(url)
+            assert response.status == 200
+            self.stdout.write("OK")
+            # content = open('test.html').read()
 
-        nodes = soup.findAll( 'a', text="View Document" )
-        for node in nodes:
-            url = node['href']
-            table = node.find_parent('table')
-            rx = re.compile(r'>([^:<]*) : ([^<]*)<')
-            data = { 'Title': table.find('b').text }
-            for match in re.finditer(rx, str(table)):
-                groups = match.groups()
-                data[groups[0]] = groups[1]
-
-            try:
-                document_date = datetime.datetime.strptime(data['Date Published'], '%d %B %Y').date()
-            except Exception as e:
-                raise CommandError( "Date could not be parsed\n%s" % str(e) )
-                # document_date = datetime.date.today()
-
-            (obj, created) = Source.objects.get_or_create(
-                document_name = data['Document Name'],
-                document_number = data['Document Number'],
-                defaults = {
-                    'url': url,
-                    'title':    data.get('Title', '(unknown)'),
-                    'language': data.get('Language', 'English'),
-                    'house':    data.get('House', '(unknown)'),
-                    'date':     document_date,
-                }
+            # parse content
+            soup = BeautifulSoup(
+                content,
+                'xml',
             )
 
-        end = end or ptotal
+            rx = re.compile(r'Displaying (\d+)  (\d+) of the most recent (\d+)')
 
-        if pend < end:
-            time.sleep(1)
-            self.retrieve_sources(pend, end)
+            pager = soup.find('td', text=rx)
+            match = rx.search(pager.text)
+            (pstart, pend, ptotal) = [int(p) for p in match.groups()]
+
+            self.stdout.write( "Processing %d to %d" % (pstart, pend) )
+
+            nodes = soup.findAll( 'a', text="View Document" )
+            def scrape(node):
+                url = node['href']
+                table = node.find_parent('table')
+                rx = re.compile(r'>([^:<]*) : ([^<]*)<')
+
+                data = {}
+                for match in re.finditer(rx, str(table)):
+                    groups = match.groups()
+                    data[groups[0]] = groups[1]
+
+                title = ''
+                try:
+                    data['Title'] = table.find('b').text
+                except:
+                    data['Title'] = data.get('Document Summary', '(unknown)')
+
+                try:
+                    document_date = datetime.datetime.strptime(data['Date Published'], '%d %B %Y').date()
+                except Exception as e:
+                    raise CommandError( "Date could not be parsed\n%s" % str(e) )
+                    # document_date = datetime.date.today()
+
+                #(obj, created) = Source.objects.get_or_create(
+                return {
+                    'document_name':   data['Document Name'],
+                    'document_number': data['Document Number'],
+                    'defaults': {
+                        'url':      url,
+                        'title':    data['Title'],
+                        'language': data.get('Language', 'English'),
+                        'house':    data.get('House', '(unknown)'),
+                        'date':     document_date,
+                    }
+                }
+            scraped = []
+            for node in nodes:
+                s = scrape(node)
+                if Source.objects.filter( 
+                    document_name   = s['document_name'], 
+                    document_number = s['document_number']).exists():
+                    if options['stop_on_seen']:
+                        return scraped
+                else:
+                    scraped.append(s)
+                    
+            if pend < (options['limit'] or ptotal):
+                time.sleep(1)
+                scraped.append( self.retrieve_sources(pend, options) )
+            return scraped
+
+        except Exception as e:
+            print >> sys.stderr, "ERROR: %s" % str(e)
+            return []

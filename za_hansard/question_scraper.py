e3e9764dd03d7087dff6167a57eaecbd894a4370
Edmund von der Burg <evdb@mysociety.org>
2013-10-08 12:26:55 +0100

    Add nicer error if pdftohtml is not found

--- za_hansard/question_scraper.py
+++ za_hansard/question_scraper.py
@@ -1,8 +1,17 @@
+import distutils
 import tempfile
+
+from django.core.exceptions import ImproperlyConfigured
 
 # from https://github.com/scraperwiki/scraperwiki-python/blob/a96582f6c20cc1897f410d522e2a5bf37d301220/scraperwiki/utils.py#L38-L54
 # Copied rather than included as the scraperwiki __init__.py was having trouble
 # loading the sqlite code, which is something we don't actually need.
+
+def ensure_executable_found(name):
+    if not distutils.spawn.find_executable(name):
+        raise ImproperlyConfigured("Can't find executable '{0}' which is needed by this code".format(name))
+
+ensure_executable_found("pdftohtml")
 def pdftoxml(pdfdata):
     """converts pdf file to xml file"""
     pdffout = tempfile.NamedTemporaryFile(suffix='.pdf')

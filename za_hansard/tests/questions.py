eb5dc78ef188789e00bfbd35162f99da8ba210e1
Edmund von der Burg <evdb@mysociety.org>
2013-10-09 10:46:01 +0100

    Reference commit of requests based approach that did not work.
    The stored response would not be suitable for easy diffing, and it is overly complicated.

--- za_hansard/tests/questions.py
+++ za_hansard/tests/questions.py
@@ -1,8 +1,49 @@
 import os
+import datetime
+
+import requests
+import requests_cache
+from UserDict import DictMixin
 
 from django.test import TestCase
 
 from .. import question_scraper
+
+class FileBasedRequestsCacheResponses(DictMixin, dict):
+
+    def cache_file(self, name):
+        tests_dir = os.path.dirname(os.path.abspath(__file__))
+        cache_dir = os.path.join(tests_dir, 'test_inputs', 'questions_cache')
+        if not os.path.exists(cache_dir):
+            os.makedirs(cache_dir)
+        return os.path.join(cache_dir, name)
+
+    def __getitem__(self, name):
+        filename = self.cache_file(name)
+        if os.path.exists(filename):
+            print "HIT  {0} --> {1}".format(name, filename)
+            response = open(filename, 'rb').read()
+            return (response, datetime.utcnow())
+        else:
+            print "MISS {0} --> {1}".format(name, filename)
+            raise KeyError
+    
+    def __setitem__(self, name, value):
+        (response, timestamp) = value
+        filename = self.cache_file(name)
+        print "SET  {0} --> {1}".format(name, filename)
+        print value
+        fh = open(filename, 'wb')
+        fh.write(response)
+        fh.close()
+
+class FileBasedRequestsCache(requests_cache.backends.base.BaseCache):
+
+    def __init__(self, *args, **kwargs):
+        print "Creating FileBasedRequestsCache"
+        self.keys_map = {}
+        self.responses = FileBasedRequestsCacheResponses()
+
 
 class ZAQuestionTests(TestCase):
 
@@ -24,3 +65,20 @@ class ZAQuestionTests(TestCase):
         # out.close()
 
         self.assertEqual(text, expected)
+
+    def test_question_detail_iterator(self):
+        # These tests should use cached data so that they are not subject to changes
+        # to the HTML on the server. This cached data is committed to the repo. If you
+        # delete the cache files they'll be regenerated on the next run, allowing you to
+        # diff any changes to the server.
+
+        requests_cache.install_cache(backend=FileBasedRequestsCache())
+        # with requests_cache.enabled(backend=FileBasedRequestsCache()):
+
+        details = question_scraper.QuestionDetailIterator("http://www.parliament.gov.za/live/content.php?Category_ID=236")
+        
+        for detail in details:
+            print detail
+            break
+        
+        self.assertTrue(False)

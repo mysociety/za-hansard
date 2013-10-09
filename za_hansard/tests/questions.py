d03f8e162477f4e5fc7c13af439dea78378208fe
Edmund von der Burg <evdb@mysociety.org>
2013-10-09 14:43:01 +0100

    Refactor tests for easier reuse

--- za_hansard/tests/questions.py
+++ za_hansard/tests/questions.py
@@ -31,7 +31,8 @@ class ZAAnswerTests(TestCase):
         self.assertEqual(text, expected)
 
 
-class ZAQuestionTests(TestCase):
+
+class ZAIteratorBaseMixin(object):
 
     def setUp(self):
         # These tests should use cached data so that they are not subject to changes
@@ -46,7 +47,7 @@ class ZAQuestionTests(TestCase):
 
     def cache_file(self, name):
         tests_dir = os.path.dirname(os.path.abspath(__file__))
-        cache_dir = os.path.join(tests_dir, 'test_inputs', 'questions_cache')
+        cache_dir = os.path.join(tests_dir, 'test_inputs', self.cache_dir_name)
         if not os.path.exists(cache_dir):
             os.makedirs(cache_dir)
         return os.path.join(cache_dir, name)
@@ -69,19 +70,23 @@ class ZAQuestionTests(TestCase):
                 write_to.write(response.text)
             return response.text
 
-    def test_question_detail_iterator(self):
-
-        details = question_scraper.QuestionDetailIterator("http://www.parliament.gov.za/live/content.php?Category_ID=236")
-
+    def fetch_details(self, details, number):
         retrieved_details = []
-        number_to_retrieve = 50
 
         with patch.object(details, "url_get", new=self.get_from_file_or_network):
             # Get the first number_to_retrieve questions
             for detail in details:
                 retrieved_details.append( detail )
-                if len(retrieved_details) >= number_to_retrieve: break
+                if len(retrieved_details) >= number: break
 
+        return retrieved_details
+
+    def test_question_detail_iterator(self):
+
+        details = self.iterator_model(self.start_url)
+        number_to_retrieve = 50
+
+        retrieved_details = self.fetch_details(details, number_to_retrieve)
 
         self.assertEqual(len(retrieved_details), number_to_retrieve)
 
@@ -89,21 +94,26 @@ class ZAQuestionTests(TestCase):
     def test_question_detail_iterator_stops_at_end(self):
 
         # Note that these tests rely on the cached html being as expected. If you update
-        # that then please change the url below to be the penultimate page of results,
-        # and the number of questions expected after scraping.
-        penultimate_list_page_url = "http://www.parliament.gov.za/live/content.php?Category_ID=236&DocumentStart=830"
-        expected_number_of_questions = 19
+        # that then please change the settings for the penultimate page of results, and
+        # the number of questions expected after scraping.
+        
+        details = self.iterator_model(self.penultimate_url)
+        number_to_retrieve = self.penultimate_expected_number + 20
 
-        details = question_scraper.QuestionDetailIterator(penultimate_list_page_url)
+        retrieved_details = self.fetch_details(details, number_to_retrieve)
 
-        retrieved_details = []
-        number_to_retrieve = expected_number_of_questions + 20
-
-        with patch.object(details, "url_get", new=self.get_from_file_or_network):
-            # Get the first number_to_retrieve questions
-            for detail in details:
-                retrieved_details.append( detail )
-                if len(retrieved_details) >= number_to_retrieve: break
+        self.assertEqual(len(retrieved_details), self.penultimate_expected_number)
 
 
-        self.assertEqual(len(retrieved_details), expected_number_of_questions)
+
+
+class ZAQuestionIteratorTests(ZAIteratorBaseMixin, TestCase):
+
+    cache_dir_name = 'questions_cache'
+    iterator_model = question_scraper.QuestionDetailIterator
+
+    start_url = "http://www.parliament.gov.za/live/content.php?Category_ID=236"
+
+    penultimate_url = start_url + "&DocumentStart=830"
+    penultimate_expected_number = 19
+

5438b7161aa0e8e2bf6e4a0f17725784e865c994
Duncan Parkes <duncan@mysociety.org>
2014-01-29 23:28:46 +0000

    Shift regex to class attribute for testing purposes.

--- za_hansard/question_scraper.py
+++ za_hansard/question_scraper.py
@@ -235,25 +235,26 @@ class QuestionPaperParser(object):
     def get_question_xml_from_pdf(self, pdfdata):
         return pdftoxml(pdfdata)
 
+
+    question_re = re.compile(
+        ur"""
+          (?P<intro>
+            (?P<number1>\d+)\.?\s+ # Question number
+            [-a-zA-z]+\s+(?P<askedby>[-\w\s]+) # Name of question asker, dropping the title
+            \s+\([\w\s]+\) # Party
+            \ to\ ask\ the\ 
+            (?P<questionto>[-\w\s(),:]+):
+            [-\w\s(),\[\]]*?
+          ) # Intro
+          (?P<translated>\u2020)?\s*</b>\s*
+          (?P<question>.*?)\s* # The question itself.
+          (?P<number2>[NC][WO]\d+E) # Number 2
+        """,
+        re.UNICODE | re.VERBOSE)
+    
     def create_questions_from_xml(self, xmldata, url):
         house = self.kwargs['house']
         date_published = datetime.datetime.strptime(self.kwargs['date'], '%d %B %Y')
-
-        question_re = re.compile(
-            ur"""
-              (?P<intro>
-                (?P<number1>\d+)\.?\s+ # Question number
-                [-a-zA-z]+\s+(?P<askedby>[-\w\s]+) # Name of question asker, dropping the title
-                \s+\([\w\s]+\)
-                \ to\ ask\ the\ 
-                (?P<questionto>[-\w\s(),:]+):
-                [-\w\s(),\[\]]*?
-              ) # Intro
-              (?P<translated>\u2020)?\s*</b>\s*
-              (?P<question>.*?)\s* # The question itself.
-              (?P<number2>[NC][WO]\d+E) # Number 2
-            """,
-            re.UNICODE | re.VERBOSE)
 
         # FIXME - can this be replaced with a call to dateutil?
         date_re = re.compile(ur"(MONDAY|TUESDAY|WEDNESDAY|THURSDAY|FRIDAY|SATURDAY|SUNDAY), \d{1,2} (JANUARY|FEBRUARY|MARCH|APRIL|MAY|JUNE|JULY|AUGUST|SEPTEMBER|OCTOBER|NOVEMBER|DECEMBER) \d{4}")
@@ -344,7 +345,9 @@ class QuestionPaperParser(object):
 
         question_paper.save()
 
-        for match in question_re.finditer(interesting_text):
+        questions = []
+
+        for match in self.question_re.finditer(interesting_text):
             match_dict = match.groupdict()
 
             match_dict[u'paper'] = question_paper

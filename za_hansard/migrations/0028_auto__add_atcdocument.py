73bad5b91e823f27fc2aad8fc4be3d280a26f0b3
Geoffrey Kilpin <geoffreykilpin@gmail.com>
2014-07-06 22:48:24 +0200

    Create ATCDocument model
    Creates the ATCDocument model, which is largely identical to the Source
    model.

---
+++ za_hansard/migrations/0028_auto__add_atcdocument.py
@@ -0,0 +1,203 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'ATCDocument'
+        db.create_table(u'za_hansard_atcdocument', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('document_name', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('document_number', self.gf('django.db.models.fields.IntegerField')(unique=True)),
+            ('date', self.gf('django.db.models.fields.DateField')()),
+            ('url', self.gf('django.db.models.fields.URLField')(max_length=1000)),
+            ('is404', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('house', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('language', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('last_processing_attempt', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
+            ('last_processing_success', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
+            ('contains_committee_announcement', self.gf('django.db.models.fields.BooleanField')(default=False)),
+        ))
+        db.send_create_signal(u'za_hansard', ['ATCDocument'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'ATCDocument'
+        db.delete_table(u'za_hansard_atcdocument')
+
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'instances.instance': {
+            'Meta': {'object_name': 'Instance'},
+            'created_by': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'created_instances'", 'null': 'True', 'to': u"orm['auth.User']"}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'label': ('instances.fields.DNSLabelField', [], {'unique': 'True', 'max_length': '63', 'db_index': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'users': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "'instances'", 'blank': 'True', 'to': u"orm['auth.User']"})
+        },
+        u'speeches.section': {
+            'Meta': {'ordering': "('id',)", 'unique_together': "(('parent', 'slug', 'instance'),)", 'object_name': 'Section'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'instance': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['instances.Instance']"}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'children'", 'null': 'True', 'to': u"orm['speeches.Section']"}),
+            'slug': ('sluggable.fields.SluggableField', [], {'unique_with': "('parent', 'instance')", 'max_length': '50', 'populate_from': "'title'"}),
+            'title': ('django.db.models.fields.TextField', [], {'blank': 'True'})
+        },
+        u'za_hansard.answer': {
+            'Meta': {'unique_together': "(('oral_number', 'house', 'year'), ('written_number', 'house', 'year'), ('president_number', 'house', 'year'), ('dp_number', 'house', 'year'))", 'object_name': 'Answer'},
+            'date': ('django.db.models.fields.DateField', [], {}),
+            'date_published': ('django.db.models.fields.DateField', [], {}),
+            'document_name': ('django.db.models.fields.TextField', [], {}),
+            'dp_number': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'db_index': 'True'}),
+            'house': ('django.db.models.fields.CharField', [], {'max_length': '1', 'db_index': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'language': ('django.db.models.fields.TextField', [], {}),
+            'name': ('django.db.models.fields.TextField', [], {}),
+            'oral_number': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'db_index': 'True'}),
+            'president_number': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'db_index': 'True'}),
+            'processed_code': ('django.db.models.fields.IntegerField', [], {'default': '0', 'db_index': 'True'}),
+            'text': ('django.db.models.fields.TextField', [], {}),
+            'type': ('django.db.models.fields.TextField', [], {}),
+            'url': ('django.db.models.fields.TextField', [], {'db_index': 'True'}),
+            'written_number': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'db_index': 'True'}),
+            'year': ('django.db.models.fields.IntegerField', [], {'db_index': 'True'})
+        },
+        u'za_hansard.atcdocument': {
+            'Meta': {'ordering': "['-date', 'document_name']", 'object_name': 'ATCDocument'},
+            'contains_committee_announcement': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'date': ('django.db.models.fields.DateField', [], {}),
+            'document_name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'document_number': ('django.db.models.fields.IntegerField', [], {'unique': 'True'}),
+            'house': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is404': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'language': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'last_processing_attempt': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'last_processing_success': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '1000'})
+        },
+        u'za_hansard.pmgcommitteeappearance': {
+            'Meta': {'object_name': 'PMGCommitteeAppearance'},
+            'committee': ('django.db.models.fields.TextField', [], {}),
+            'committee_url': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'meeting': ('django.db.models.fields.TextField', [], {}),
+            'meeting_date': ('django.db.models.fields.DateField', [], {}),
+            'meeting_url': ('django.db.models.fields.TextField', [], {}),
+            'party': ('django.db.models.fields.TextField', [], {}),
+            'person': ('django.db.models.fields.TextField', [], {}),
+            'report': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'appearances'", 'null': 'True', 'to': u"orm['za_hansard.PMGCommitteeReport']"}),
+            'text': ('django.db.models.fields.TextField', [], {})
+        },
+        u'za_hansard.pmgcommitteereport': {
+            'Meta': {'object_name': 'PMGCommitteeReport'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_sayit_import': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'meeting_url': ('django.db.models.fields.TextField', [], {}),
+            'premium': ('django.db.models.fields.BooleanField', [], {}),
+            'processed': ('django.db.models.fields.BooleanField', [], {}),
+            'sayit_section': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['speeches.Section']", 'null': 'True', 'on_delete': 'models.PROTECT', 'blank': 'True'})
+        },
+        u'za_hansard.question': {
+            'Meta': {'unique_together': "(('written_number', 'house', 'year'), ('oral_number', 'house', 'year'), ('president_number', 'house', 'year'), ('dp_number', 'house', 'year'), ('id_number', 'house', 'year'))", 'object_name': 'Question'},
+            'answer': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'question'", 'null': 'True', 'to': u"orm['za_hansard.Answer']"}),
+            'answer_type': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'askedby': ('django.db.models.fields.TextField', [], {}),
+            'date': ('django.db.models.fields.DateField', [], {}),
+            'date_transferred': ('django.db.models.fields.DateField', [], {'null': 'True'}),
+            'dp_number': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'db_index': 'True'}),
+            'house': ('django.db.models.fields.CharField', [], {'max_length': '1', 'db_index': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'id_number': ('django.db.models.fields.IntegerField', [], {'db_index': 'True'}),
+            'identifier': ('django.db.models.fields.CharField', [], {'max_length': '10', 'db_index': 'True'}),
+            'intro': ('django.db.models.fields.TextField', [], {}),
+            'last_sayit_import': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'oral_number': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'db_index': 'True'}),
+            'paper': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['za_hansard.QuestionPaper']", 'null': 'True', 'on_delete': 'models.SET_NULL'}),
+            'president_number': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'db_index': 'True'}),
+            'question': ('django.db.models.fields.TextField', [], {}),
+            'questionto': ('django.db.models.fields.TextField', [], {}),
+            'sayit_section': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['speeches.Section']", 'null': 'True', 'on_delete': 'models.PROTECT', 'blank': 'True'}),
+            'translated': ('django.db.models.fields.BooleanField', [], {}),
+            'written_number': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'db_index': 'True'}),
+            'year': ('django.db.models.fields.IntegerField', [], {'db_index': 'True'})
+        },
+        u'za_hansard.questionpaper': {
+            'Meta': {'unique_together': "(('year', 'issue_number', 'house', 'parliament_number'),)", 'object_name': 'QuestionPaper'},
+            'date_published': ('django.db.models.fields.DateField', [], {}),
+            'document_name': ('django.db.models.fields.TextField', [], {'max_length': '32'}),
+            'document_number': ('django.db.models.fields.IntegerField', [], {}),
+            'house': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'issue_number': ('django.db.models.fields.IntegerField', [], {}),
+            'language': ('django.db.models.fields.CharField', [], {'max_length': '16'}),
+            'parliament_number': ('django.db.models.fields.IntegerField', [], {}),
+            'session_number': ('django.db.models.fields.IntegerField', [], {}),
+            'source_url': ('django.db.models.fields.URLField', [], {'max_length': '1000'}),
+            'text': ('django.db.models.fields.TextField', [], {}),
+            'year': ('django.db.models.fields.IntegerField', [], {})
+        },
+        u'za_hansard.source': {
+            'Meta': {'ordering': "['-date', 'document_name']", 'object_name': 'Source'},
+            'date': ('django.db.models.fields.DateField', [], {}),
+            'document_name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'document_number': ('django.db.models.fields.IntegerField', [], {'unique': 'True'}),
+            'house': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is404': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'language': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'last_processing_attempt': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'last_processing_success': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'last_sayit_import': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'sayit_section': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['speeches.Section']", 'null': 'True', 'on_delete': 'models.PROTECT', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '1000'})
+        }
+    }
+
+    complete_apps = ['za_hansard']
--- za_hansard/models.py
+++ za_hansard/models.py
@@ -436,3 +436,121 @@ class Question(models.Model):
         # 1) At least one of written_number and oral_number must be non-null.
 
 #CREATE TABLE completed_documents (`url` string);
+
+class ATCDocument(models.Model):
+    """
+    ZA Parliament Announcement, Tablings and Committee Reports.
+    """
+
+    title           = models.CharField(max_length=200)
+    document_name   = models.CharField(max_length=200)
+    document_number = models.IntegerField(unique=True)
+    date            = models.DateField()
+    url             = models.URLField(max_length=1000)
+    is404           = models.BooleanField(default=False)
+    house           = models.CharField(max_length=200)
+    language        = models.CharField(max_length=200)
+
+    last_processing_attempt = models.DateTimeField(blank=True, null=True)
+    last_processing_success = models.DateTimeField(blank=True, null=True)
+
+    contains_committee_announcement = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = [ '-date', 'document_name' ]
+
+
+    def __unicode__(self):
+        return self.document_name
+
+
+    def delete(self):
+        """After deleting from db, delete the cached file too"""
+        cache_file_path = self.cache_file_path()
+        super( Source, self ).delete()
+
+        if os.path.exists( cache_file_path ):
+            os.remove( cache_file_path )
+
+
+    def file(self, debug=False):
+        """
+        Return as a file object the resource that the url is pointing to.
+
+        Should check the local cache first, and fetch and store if it is not
+        found there.
+
+        Raises a SourceUrlCouldNotBeRetrieved exception if URL could not be
+        retrieved.
+        """
+        cache_file_path = self.cache_file_path()
+
+        found = os.path.isfile(cache_file_path)
+
+        if debug:
+            print >> sys.stderr, "%s (%s)" % (cache_file_path, found)
+
+        # If the file exists open it, read it and return it
+        if found:
+            return cache_file_path
+
+        # If not fetch the file, save to cache and then return fh
+        h = httplib2.Http()
+        url = 'http://www.parliament.gov.za/live/' + self.url
+
+        def request_url(url):
+            if debug:
+                print >> sys.stderr, 'Requesting %s' % url
+            (response, content) = h.request(url)
+            if response.status != 200:
+                raise SourceUrlCouldNotBeRetrieved("status code: %s, url: %s" % (response.status, self.url) )
+            self.is404 = False
+            self.save()
+            return (response, content)
+
+        try:
+            (response, content) = request_url(url)
+        except SourceUrlCouldNotBeRetrieved as e:
+            try:
+                if not url[-4:] == '.doc':
+                    (response, content) = request_url(url + '.doc')
+                    self.url = self.url + '.doc'
+                    self.save()
+                else:
+                    raise e
+            except:
+                raise e
+
+        if not content:
+            raise SourceUrlCouldNotBeRetrieved("WTF?")
+        with open(cache_file_path, "w") as new_cache_file:
+            new_cache_file.write(content)
+
+        return cache_file_path
+
+    def cache_file_path(self):
+        """Absolute path to the cache file for this source"""
+
+        id_str= "%05u" % self.id
+
+        # do some simple partitioning
+        # FIXME - put in something to prevent the test suite overwriting non-test files.
+        aaa = id_str[-1]
+        bbb = id_str[-2]
+        cache_dir = os.path.join(settings.HANSARD_CACHE, aaa, bbb)
+
+        # check that the dir exists
+        if not os.path.exists( cache_dir ):
+            os.makedirs( cache_dir )
+
+        d = self.date.strftime('%Y-%m-%d')
+
+        # create the path to the file
+        cache_file_path = os.path.join(cache_dir, '-'.join([d, id_str, self.document_name]))
+        return cache_file_path
+
+    def xml_file_path(self):
+        xml_file_path = '%s.xml' % self.cache_file_path()
+        if os.path.isfile(xml_file_path):
+            return xml_file_path
+        return None

a5b88c0bf20adbffb10bc7bd396e43476015e41a
Greg Kempe <greg@kempe.net>
2015-06-19 12:13:18 +0200

    Allow user to edit answer text inline

--- za_hansard/admin.py
+++ za_hansard/admin.py
@@ -4,26 +4,51 @@ from django.contrib import admin, messages
 from django.core import urlresolvers
 from django.conf.urls import patterns, url
 from django.shortcuts import redirect
+from django import forms
 
 from za_hansard.models import Question
 from za_hansard import question_scraper
+
+
+class CustomQuestionForm(forms.ModelForm):
+    answer_text = forms.CharField(required=False, widget=forms.widgets.Textarea(attrs={
+        'class': 'vLargeTextField',
+    }))
+
+    def __init__(self, *args, **kwargs):
+        super(CustomQuestionForm, self).__init__(*args, **kwargs)
+        instance = kwargs.get('instance')
+        if instance:
+            if instance.answer:
+                self.initial['answer_text'] = instance.answer.text
+            else:
+                self.fields['answer_text'].widget.attrs['disabled'] = True
+
+    def save(self, commit=True):
+        instance = super(CustomQuestionForm, self).save(commit)
+        if instance.answer:
+            instance.answer.text = self.cleaned_data['answer_text']
+            instance.answer.save()
+
+        return instance
 
 
 class QuestionAdmin(admin.ModelAdmin):
     date_hierarchy = 'date'
     list_display = ('date', 'house', 'written_number', 'oral_number', 'president_number', 'dp_number', 'answered')
     list_filter = ('house',)
+    form = CustomQuestionForm
 
     fieldsets = (
         (None, {
-            'fields': ('date', 'year', 'house', 'written_number', 'oral_number', 'president_number', 'dp_number',
-                       'identifier', 'id_number'),
+            'fields': (('date', 'year'), 'house', 'written_number', 'oral_number', 'president_number', 'dp_number',
+                       ('identifier', 'id_number')),
         }),
         ('Question', {
             'fields': ('intro', 'askedby', 'questionto', 'question', ),
         }),
         ('Answer', {
-            'fields': ('answer_type', 'answer', ),
+            'fields': ('answer_text', 'answer_type', 'answer'),
         }),
         ('Advanced', {
             'classes': ('collapse',),

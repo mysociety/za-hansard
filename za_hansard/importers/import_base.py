78220ba97303c2c3de3cfecf8e17d38e84e5c5b8
Mark Longair <mhl@pobox.com>
2016-10-05 16:25:14 +0100

    Add a method for helping to analyze name resolution results
    It's useful to compare whether our name resolution code would get the
    same as the PA person that's named in the Code4SA results; adding calls
    to this method in get_person appends rows to a CSV file.

--- za_hansard/importers/import_base.py
+++ za_hansard/importers/import_base.py
@@ -42,6 +42,20 @@ class ImportZAMixin(object):
             person_filter=AllowedPersonFilter(self.pombola_id_blacklist),
         )
 
+    def debug_output_csv_row(self, pombola_person_slug, from_cache, our_speaker, speaker_from_slug):
+        '''A method to help produce data for analyzing name resolution results'''
+        import csv
+        with open('name-resolution.log', 'ab') as f:
+            writer = csv.writer(f)
+            writer.writerow([
+                str(self.resolver.date),
+                '' if pombola_person_slug is None else pombola_person_slug,
+                our_speaker.id if our_speaker else '',
+                our_speaker.name.encode('utf-8') if our_speaker else '',
+                speaker_from_slug.id if speaker_from_slug else '',
+                speaker_from_slug.name.encode('utf-8') if speaker_from_slug else '',
+            ])
+
     def get_person(self, name, party):
         cached = self.person_cache.get(name, None)
         if cached:

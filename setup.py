6bde60421e44a45a8d2d5921ddc510b23c63a954
Chris Mytton <chrismytton@gmail.com>
2019-06-10 18:24:36 +0100

    Remove requirements from setup.py
    These are all defined in pombola anyway, and having them redefined here
    just leads to dependency hell. Instead just leave the install_requires
    entry empty, then if someone for some reason wants to run this
    standalone then they can install the dependencies from requirements.txt.

--- setup.py
+++ setup.py
@@ -7,24 +7,18 @@ def read_file(filename):
     filepath = os.path.join(file_dir, filename)
     return open(filepath).read()
 
-def install_requires():
-    reqs = read_file('requirements.txt')
-    reqs = reqs.splitlines()
-    reqs = [ x for x in reqs if x and x[0] != '#' and x[0:2] != '-e' ]
-    return reqs
-
 setup(
     name="za-hansard",
     version='0.2',
     description='A scraper and parser for South African parliamentary transcripts.',
     long_description=read_file('README.md'),
     author='mySociety',
-    author_email='hakim@mysociety.org',
+    author_email='modules@mysociety.org',
     url='https://github.com/mysociety/za-hansard',
     packages=find_packages(exclude=["example_project"]),
     package_data={'za_hansard': ['tests/test_inputs/*/*/*/*']},
     include_package_data=True,
-    install_requires=install_requires(),
+    install_requires=[],
     classifiers=[
         'Framework :: Django',
     ],
